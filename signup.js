243a38fbd1d2ce970f5ce9ab364d58b5ec0db710
Alex Chief <alowe.21407@gmail.com>
2022-11-21 11:08:52 -0500

    Continued work on sign up and log in page. Hardcoded manager login information. Minor organizational changes to sign up and log in HTML pages. Updated conditionals in sign up function and added connection to local storage.

--- signup.js
+++ signup.js
@@ -1,36 +1,51 @@
-function myfunction() {
+function signUp() {
     let length = document.getElementById("form").childElementCount;
-    let account = "";
-    for(let i=0; i<length; i++) { // Iterates through all children of form div
-        if (i%2===1) { // This conditional selects input boxes and not labels
-            if (i<5) {// Conditional to select first two input boxes
-                if (document.getElementById("form").children[i].value.length > 1) {
-                    // Conditional that ensures names are at least 2 characters
-                    console.log("name" + document.getElementById("form").children[i].value);
+    let infoArray =[];
+    let userEmail = "";
+    for(let i=0; i<length; i++) {
+        if (i%2===1) {
+            if (i===1) {
+                if (document.getElementById("form").children[i].value.length > 4) {
+                    userEmail = document.getElementById("form").children[i].value;
+                }
+                else {
+                    console.log("Email too short");
                 }
             }
-            let info = [];
-            if (document.getElementById("form").children[i].value.length > 1) {
-                // This conditional verifies that the inputs have at least two characters
-                console.log(document.getElementById("form").children[i].value);
-            }
-            else {
-                // Highlight text box that triggered this event
-                console.log("All fields ");
+            else if (i>4 && i<8) {
+                if (document.getElementById("form").children[i].value.length > 1) {
+                    infoArray.push(document.getElementById("form").children[i].value);
+                }
+                else {
+                    console.log(`${i} is not long enough.`)
+                }            
+            } 
+            else if (i>8) {
+                if (document.getElementById("form").children[i].value.length > 4) {
+                    infoArray.push(document.getElementById("form").children[i].value);
+                }
+                else {
+                    console.log(`${i} is not long enough.`)
+                }          
             }
         }
     }
+    // localStorage.setItem(userEmail, infoArray);
+    // Redirect to landing page here
+    // window.location.href = "";
 }
-function fun() {
-    console.log("");
+
+function logIn(){
+    console.log(localStorage.getItem(document.getElementById("usernameLogIn").value));
+    if (document.getElementById("usernameLogIn").value === "gustavo" && document.getElementById("passwordLogIn").value === "venganza"){
+        console.log("Manager Login Successful");
+        // window.location.href = "";
+    }
+    else {
+        console.log(localStorage.getItem(document.getElementById("usernameLogIn").value));
+    }
 }
 
-// Store account info in an array
-// Push info to local storage with email as key and array as value
-// Make local storage accessible to mutliple pages?
-
-
-// For Loop to iterate through all children of the form div
-// Conditional that first specifies only the text input boxes
-// Then checks field lengths of required text box fields
-// Then differentiate between email and not email to make email the key (of key/value pair)
+localStorage.setItem("gustavo", "venganza");
+// username: adlowe221@stevenscollege.edu
+// Password: Password
