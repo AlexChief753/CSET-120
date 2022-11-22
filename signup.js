543cfebdd5213cd3fc006afa5447b22580cb49ea
Alex Chief <alowe.21407@gmail.com>
2022-11-22 11:32:37 -0500

    Completed javascript work for Log In Page, including required fields, account creation in local storage, inability to create duplicate accounts. Customers must have an email, password, first and last name, and phone number to create an account. Delivery address is optional but stored as blank if unanswered.

--- signup.js
+++ signup.js
@@ -1,51 +1,47 @@
-function signUp() {
+    let form = document.getElementById("form");
     let length = document.getElementById("form").childElementCount;
-    let infoArray =[];
+function signUp() {
     let userEmail = "";
-    for(let i=0; i<length; i++) {
-        if (i%2===1) {
-            if (i===1) {
-                if (document.getElementById("form").children[i].value.length > 4) {
-                    userEmail = document.getElementById("form").children[i].value;
+    let infoArray =[];
+    let status = true;
+    let managerStatus = false;
+    for(let i=0; i<length; i++){
+        if(i%2===1){
+            if (i<10 && form.children[i].value.length <= 3){
+                alert(`${form.children[i-1].innerHTML.replace(":", "")} is too short. Please use a minimum of 4 characters.`);
+                status = false;
+            }
+            if (i===1){
+                if (localStorage.getItem(form.children[i].value) !== null){
+                    alert("This email is associated existing account. Please use a different email or try logging in.");
+                    status = false;
                 }
-                else {
-                    console.log("Email too short");
+                else{
+                    userEmail = form.children[i].value;
                 }
             }
-            else if (i>4 && i<8) {
-                if (document.getElementById("form").children[i].value.length > 1) {
-                    infoArray.push(document.getElementById("form").children[i].value);
-                }
-                else {
-                    console.log(`${i} is not long enough.`)
-                }            
-            } 
-            else if (i>8) {
-                if (document.getElementById("form").children[i].value.length > 4) {
-                    infoArray.push(document.getElementById("form").children[i].value);
-                }
-                else {
-                    console.log(`${i} is not long enough.`)
-                }          
+            if (i!==1){
+                infoArray.push(form.children[i].value);
             }
         }
     }
-    // localStorage.setItem(userEmail, infoArray);
-    // Redirect to landing page here
-    // window.location.href = "";
-}
-
-function logIn(){
-    console.log(localStorage.getItem(document.getElementById("usernameLogIn").value));
-    if (document.getElementById("usernameLogIn").value === "gustavo" && document.getElementById("passwordLogIn").value === "venganza"){
-        console.log("Manager Login Successful");
+    if(status === true) {
+        console.log(userEmail);
+        console.log(infoArray);   
+        localStorage.setItem(userEmail, infoArray);
         // window.location.href = "";
     }
-    else {
-        console.log(localStorage.getItem(document.getElementById("usernameLogIn").value));
-    }
 }
 
+// function logIn(){
+//     if(.children[1].value === "gustavo" && form.children[3].value === "venganza"){
+//         console.log("Manager Login Successful.")
+//         managerStatus = true;
+//         // window.location.href ="";
+//     }
+// }
+
+
+
+
 localStorage.setItem("gustavo", "venganza");
-// username: adlowe221@stevenscollege.edu
-// Password: Password
