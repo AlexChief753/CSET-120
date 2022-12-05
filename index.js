19505a437ee45775f72f934a737d44f8cda7d8f4
Alex Chief <alowe.21407@gmail.com>
2022-12-05 09:09:47 -0500

    Finished function to edit images of existing menu items, as well as one to revert the new image to the initially set image.

--- index.js
+++ index.js
@@ -113,27 +113,41 @@ function imgReplace(parameter){
 }
 
 function changeButton(param){
+    let imageOriginal = param.parentElement.children[Array.from(param.parentElement.children).indexOf(param)-2].src;
     let textAreaInput = param.parentElement.children[Array.from(param.parentElement.children).indexOf(param)-1].value;
-    param.parentElement.children[Array.from(param.parentElement.children).indexOf(param)-2].src = textAreaInput;
-    sessionStorage.setItem("lastAction", textAreaInput);
-    // console.log(sessionStorage.getItem("lastAction"));
+    let menuIterative = document.getElementById("menu");
+    for (i=1; i<menuIterative.childElementCount; i++){
+        let menuRow = menuIterative.children[i];
+        for(n=0; n<menuRow.childElementCount; n++){
+            let menuItem = menuRow.children[n];
+            if(menuItem.contains(param) == true){
+                sessionStorage.setItem(i + `${n+1}`, imageOriginal);
+            }
+        }
+    }
     param.innerHTML = "Undo";
-    // console.log(param.onclick);
-    param.setattribute("onclick","undoButton();");
-    // console.log(param.onclick);
-
-    // use .item() to get index number, subtract from it
-    // store values; session storage? update button innerhtml and onclick attributes
+    param.setAttribute("onclick", "undoButton(this);");
+    param.parentElement.children[Array.from(param.parentElement.children).indexOf(param)-2].src = textAreaInput;
 }
 
-// Strangest thing: referring to a child of an HTML Collection using: 
-// Brackets[index] - will return undefined if index is not a number
-// .item() method - will convert the argument to a number and return corresponding element
-// ...Even though index brackets are supposed to just be shorthand for .item()
+function undoButton(param){
+    let image = param.parentElement.children[Array.from(param.parentElement.children).indexOf(param)-2];
+    let menuIterative = document.getElementById("menu");
+    for (i=1; i<menuIterative.childElementCount; i++){
+        let menuRow = menuIterative.children[i];
+        for(n=0; n<menuRow.childElementCount; n++){
+            let menuItem = menuRow.children[n];
+            if(menuItem.contains(param) == true){
+                image.src = sessionStorage.getItem(i+`${n+1}`);
+            }
+        }
+    }
+    param.innerHTML = "Preview Image";
+    param.setAttribute("onclick", "changeButton(this);");
+}
 
-function undoButton(paaram){
-    console.log("Undo Function called");
-    // access stored values, update image src, update button innerhtml and onclick attributes
+function buttonTwo(){
+    console.log("Button two clicked");
 }
 
 // Calling functions
